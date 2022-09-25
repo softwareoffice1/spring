86e86cb0f6afa410ef5304f5c7f05a280e560619
Juan Gonzalez <juanchoes.gon@gmail.com>
2022-09-25 00:22:41 -0500

    test

---
+++ src/test/java/com/softwareoffice/proyect/ProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareoffice.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
