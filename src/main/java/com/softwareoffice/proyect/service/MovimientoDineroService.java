309636d924de724d63ad380aa4771a624cf7cfd5
darwinsossa <dsossa60@uan.edu.co>
2022-09-25 21:00:18 -0500

    se realiza metodos de movimiento de dinero. att darwinsossa

--- src/main/java/com/softwareoffice/proyect/service/MovimientoDineroService.java
+++ src/main/java/com/softwareoffice/proyect/service/MovimientoDineroService.java
@@ -1,4 +1,11 @@
 package com.softwareoffice.proyect.service;
 
+import com.salesForce.entity.MovimientoDinero;
+import com.salesForce.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+import javax.persistence.EntityNotFoundException;
+import java.sql.Timestamp;
+import java.util.List;
+@Service 
 public class MovimientoDineroService {
 }
