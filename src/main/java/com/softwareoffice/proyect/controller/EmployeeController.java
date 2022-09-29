b1eab9349e02cce9b339cdef5574afe8c74c7b17
ingelect <alexisssoriano@hotmail.com>
2022-09-28 22:54:15 -0500

    nuevo

--- src/main/java/com/softwareoffice/proyect/controller/EmployeeController.java
+++ src/main/java/com/softwareoffice/proyect/controller/EmployeeController.java
@@ -12,23 +12,17 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    // El sistema develve responses 200 en la rutau /enterprises con los siguientes verbos:  |GET|POST|
-    //Se puede crear un empleado segun post
-
     @PostMapping("/user")
     public List<Employee> crearEmpleado (@RequestBody Employee employee){
         employeeService.crearEmpleado (employee);
         return employeeService.consultarEmpleados ();
     }
 
-    //Se puede consultar la lista de empleados
     @GetMapping("/user")
     public List<Employee> consultarEmpleados (){
         List<Employee> employee = employeeService.consultarEmpleados();
         return employee;
     }
-
-    // El sistema devuelve responses 200 en la ruta /enterprises/[id] con los siguientes verbos: |GET|PATCH|DELETE|
 
     @GetMapping("/user/{id_empleado}")
     public  Employee consultarEmpleado (@PathVariable("id_empleado") Long id_Empleado) {
