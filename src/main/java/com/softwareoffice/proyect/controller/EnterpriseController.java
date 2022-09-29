5ba21b074e9799cd7e1e1c05d790318b047b939b
ingelect <alexisssoriano@hotmail.com>
2022-09-28 22:55:21 -0500

    nuevo

--- src/main/java/com/softwareoffice/proyect/controller/EnterpriseController.java
+++ src/main/java/com/softwareoffice/proyect/controller/EnterpriseController.java
@@ -23,21 +23,18 @@ public class EnterpriseController {
         return enterpriseService.findEnterprises();
     }
 
-    // Crea una empresa
-    @PostMapping("/enterprises")
+     @PostMapping("/enterprises")
     public RedirectView createEmpresa (@ModelAttribute Enterprise enterprise, Model model){
         model.addAttribute(enterprise);
         this.enterpriseService.createEnterprise(enterprise);
         return new RedirectView("/empresas");
     }
 
-    // Busca la empresa segun id_empresa
     @GetMapping("/enterprises/{id_enterprise}")
     public Enterprise findEnterprise (@PathVariable ("id_enterprise") Long id_enterprise){
         return enterpriseService.findEnterprise(id_enterprise);
     }
 
-    // Actualiza el nit de la Empresa
     @PatchMapping("/enterprises/{id_enterprise}/{document}")
     public Enterprise editDocument (@PathVariable ("id_enterprise") Long id, @PathVariable ("document") String document){
         enterpriseService.editDocument(id, document);
